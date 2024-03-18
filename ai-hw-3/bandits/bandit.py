de6bd9046a579f798f106c09b57cc20f8963bce8
David Hidary <davidrhidary@gmail.com>
2024-03-17 21:57:56 -0400

    initial bandit implementation

--- ai-hw-3/bandits/bandit.py
+++ ai-hw-3/bandits/bandit.py
@@ -31,32 +31,49 @@ class Bandit:
     def simulate(
         self
     ) -> Tuple[npt.NDArray, npt.NDArray]:
-        # TODO
         regret = np.zeros(self.N)
+        if self.strategy == 0:
+            self.choose_arm = self.choose_arm_egreedy
+        elif self.strategy == 1:
+            self.choose_arm = self.choose_arm_edecay
+        else:
+            self.choose_arm = self.choose_arm_ucb
+        for t in range(self.N):
+            idx = self.choose_arm()
+            reward = self.pull_arm(idx)
+            self.arm_counts[idx] += 1
+            self.Qvalues[idx] += (reward - self.Qvalues[idx]) / self.arm_counts[idx]
+            if np.argmax(self.Qvalues) != idx:
+                regret[t] = self.get_path_mean(idx) - self.get_path_mean(np.argmax(self.Qvalues))
+            else:
+                regret[t] = 0
         return self.Qvalues, regret
 
 
     def choose_arm_egreedy(
         self
     ) -> int:
-        # TODO
-        return 0
+        if np.random.rand() < self.value:
+            return np.argmax(self.Qvalues)
+        else:
+            return np.random.randint(len(self.arms))
 
 
     def choose_arm_edecay(
         self,
         t: int
     ) -> int:
-        # TODO
-        return 0
+        if np.random.rand() < min(1, (self.arm_counts * self.value) / (t + 1)):
+            return np.argmax(self.Qvalues)
+        else:
+            return np.random.randint(len(self.arms))
 
 
     def choose_arm_ucb(
         self,
         t: int
     ) -> int:
-        # TODO
-        return 0
+        return np.argmax(self.Qvalues + self.value * np.sqrt(np.log(t + 1) / self.arm_counts))
 
 
     def pull_arm(
